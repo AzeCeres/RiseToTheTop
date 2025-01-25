da12e0c67294658f69a9f646ed0bdbd84700b75e
AzeCeres <91120004+AzeCeres@users.noreply.github.com>
2025-01-25 14:02:40 +0100

    Updated Input
    Updated Input to reflect the changes in the input actions. Made the functions with statement bodies to allow for more logic inside of the functions

--- Assets/Script/Player/Input.cs
+++ Assets/Script/Player/Input.cs
@@ -10,7 +10,15 @@ public class Input : MonoBehaviour
         moveDirection = value.Get<Vector2>();
     }
     public Vector2 lookVector { get; private set; }
-    public void OnLook(InputValue value) => lookVector = value.Get<Vector2>();
+    public void OnLookJoypad(InputValue value)
+    {
+        lookVector = value.Get<Vector2>();
+    }
+    public void OnLookMouse(InputValue value)
+    {
+        //Might need a different vector for the mouse, might not be between -1 and 1 like the joypad
+        lookVector = value.Get<Vector2>();
+    }
     public bool isShooting { get; private set; }
     public void OnShoot(InputValue value)
     {
